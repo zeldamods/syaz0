063efd871a823e6123403ecb983fa348f9b0f1ee
Léo Lam <leo@leolam.fr>
2019-12-26 17:19:23 +0100

    py: Take buffers for `compress`

--- src/main.cpp
+++ src/main.cpp
@@ -62,8 +62,8 @@ PYBIND11_MODULE(syaz0, m) {
       "data"_a);
   m.def(
       "compress",
-      [](py::bytes src_py, u32 data_alignment, int level) {
-        const auto src = detail::PyBytesToSpan(src_py);
+      [](py::buffer src_py, u32 data_alignment, int level) {
+        const auto src = detail::PyBufferToSpan(src_py);
         return syaz0::Compress(src, data_alignment, level);
       },
       "data"_a, "data_alignment"_a = 0, "level"_a = 7);
